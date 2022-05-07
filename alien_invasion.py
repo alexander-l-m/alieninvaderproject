ed598b26da7ed2287ed33b2391f3591cd90b510c
alexander-l-m <alexanderlmccall@gmail.com>
2022-05-07 13:42:41 +1000

    Rearranged code into a couple of additionally created methods to avoid clutter in the run_game class, messed around with background colours, and added a second image to the background as part of an exercise

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,5 +1,6 @@
 import sys
 import pygame
+from ship import BackgroundCharacter
 from settings import Settings
 from ship import Ship
 
@@ -16,27 +17,41 @@ class AlienInvastion:
         pygame.display.set_caption("Alien Invasion")
 
         # Set the background colour.
-        self.bg_color = (230, 230, 230)
+        self.bg_color = (0, 0, 0)
 
         # Applying the ship to the main class
         self.ship = Ship(self)
+        self.alien = BackgroundCharacter(self)
 
+    
     def run_game(self):
         # Start the main loop for the game
         while True:
-            # Watch for keyboard and mouse events
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-
-            # Redraw the screen during each pass through the loop
-            self.screen.fill(self.settings.bg_color)
-
-            # Placing the ship to the instance of the game
-            self.ship.blitme()
+            self._check_events()
+            self._update_screen()
             
-            # Make the most recently drawn screen visable
-            pygame.display.flip()
+
+            
+    def _check_events(self):
+        
+        # Respond to key presses and mouse events 
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+    
+    def _update_screen(self):
+
+        # Redraw the screen during each pass through the loop
+        self.screen.fill(self.settings.bg_color)
+
+        # Placing the ship to the instance of the game
+        self.alien.blitme()
+        self.ship.blitme()
+            
+        # Make the most recently drawn screen visable
+        pygame.display.flip()
+
+
 
 if __name__ == '__main__':
     # Make a game instance, and run the game
--- ship.py
+++ ship.py
@@ -11,6 +11,7 @@ class Ship:
 
         # Load the ship image and get its rect
         self.image = pygame.image.load('alien_invasion/images/ship.bmp')
+        
         self.rect = self.image.get_rect()
 
         # Start each new ship at the bottom centre of the screen
@@ -19,4 +20,25 @@ class Ship:
     def blitme(self):
         # Draw the ship at its current location.
         self.screen.blit(self.image, self.rect)
+
+    
+
+class BackgroundCharacter:
+
+    def __init__(self, ai_game):
+        # Initliase the background character and place it in the middle of the screen  
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+
+        # Loading the background image
+        self.image2 = pygame.image.load('alien_invasion/images/alien.bmp')
+        self.rect = self.image2.get_rect()
+
+        # Start character in the middle of the screen
+        self.rect.midtop = self.screen_rect.midtop
+    
+    def blitme(self):
+        # Draw the ship at its current location.
+        self.screen.blit(self.image2, self.rect)
+        
  
