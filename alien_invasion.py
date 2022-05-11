1468e5698b0a49476e13431c3fce5c5bd2782bd2
alexander-l-m <alexanderlmccall@gmail.com>
2022-05-11 18:24:45 +1000

    Added the play button to the screen. Had to make a method for the mouse position(_check_play_button in alien_invasion.py), so whilst the game state is false, the button will appear. The user has the option to click it, the rect button disappears, but so does the curser once the user has clicked Play. The check_play button also allows the game to restart after the maximum amount of ships a player is allowed decrements to 0. After the player runs out of lives, the start rect button appears again.

--- alien_invasion.py
+++ alien_invasion.py
@@ -8,6 +8,7 @@ from settings import Settings
 from game_stats import GameStats
 from bullet import Bullet
 from alien import Alien
+from button import Button
 
 
 class AlienInvasion:
@@ -32,6 +33,9 @@ class AlienInvasion:
         self.aliens = pygame.sprite.Group()
 
         self._create_fleet()    
+        
+        # Make the play button
+        self.play_button = Button(self, "Play")
 
     
     def run_game(self):
@@ -44,7 +48,7 @@ class AlienInvasion:
                  
                 self._update_bullets()
                 self._update_aliens()
-                
+
             self._update_screen() 
 
 
@@ -106,7 +110,30 @@ class AlienInvasion:
                 self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event)
-            
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = pygame.mouse.get_pos()
+                self._check_play_button(mouse_pos)
+
+
+    def _check_play_button(self, mouse_pos):
+        # Start a new game when the player clicks play
+        button_clicked = self.play_button.rect.collidepoint(mouse_pos)
+        if button_clicked and not self.stats.game_active:
+
+            # Reset the game statistics
+            self.stats.reset_stats()
+            self.stats.game_active = True
+
+            # Get rid of any remaining aliens and bullets
+            self.aliens.empty()
+            self.bullets.empty()
+
+            # Create a new fleet and centre the ship
+            self._create_fleet()
+            self.ship.center_ship()
+
+            # Hide the mouse cursor.
+            pygame.mouse.set_visible(False)
 
             
     def _check_keydown_events(self, event):
@@ -216,11 +243,8 @@ class AlienInvasion:
             sleep(0.5)
         else:
             self.stats.game_active = False
+            pygame.mouse.set_visible(True)
 
-    def center_ship(self):
-        # Centre the ship on the screen
-        self.rect.midbottom = self.screen_rect.midbottom
-        self.x = float(self.rect.x)
 
     
     def _update_screen(self):
@@ -236,6 +260,10 @@ class AlienInvasion:
             bullet.draw_bullet()
 
         self.aliens.draw(self.screen)
+
+        # Draw the play button if the game is inactive.
+        if not self.stats.game_active:
+            self.play_button.draw_button()
             
         # Make the most recently drawn screen visable
         pygame.display.flip()    
